6ac14b800a0683f520355aa827119a10ab1400ea
maria-jahan20 <mariajahan4596@gmail.com>
2025-10-25 01:31:02 +0600

    management, property and about

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -5,13 +5,6 @@ import Testimonial from "./testimonial";
 export default function FooterPage() {
   return (
     <main className="min-h-screen flex flex-col">
-      {/* Your page content goes here */}
-      <div className="flex-1 flex items-center justify-center bg-gray-50">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Welcome</h1>
-          <p className="text-gray-600">Your content here</p>
-        </div>
-      </div>
 
       <Testimonial />
 
